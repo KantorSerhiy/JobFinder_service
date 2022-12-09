e3a50dc96cceac2bd6531391a636cd63142a48b7
skantor <serhiykantor@gmail.com>
2022-12-09 22:14:44 +0200

    Create model City & register in admins

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, blank=True)
+
+    class Meta:
+        verbose_name = "City"
+        verbose_name_plural = "Cities"
+
+    def __str__(self):
+        return self.name
+
