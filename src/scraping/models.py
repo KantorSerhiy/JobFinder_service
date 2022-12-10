296f5c84a1b191a5c5bb8f54a249d755e6f8affa
skantor <serhiykantor@gmail.com>
2022-12-10 12:07:45 +0200

    add model Language

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -2,12 +2,24 @@ from django.db import models
 
 
 class City(models.Model):
-    name = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=50, blank=True)
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, blank=True, unique=True)
 
     class Meta:
         verbose_name = "City"
         verbose_name_plural = "Cities"
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = "Language"
+        verbose_name_plural = "Languages"
 
     def __str__(self):
         return self.name
