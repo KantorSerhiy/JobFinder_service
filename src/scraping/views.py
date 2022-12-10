49301a4b078b9b3770b5ba9cac3535988732ea16
skantor <serhiykantor@gmail.com>
2022-12-10 17:32:20 +0200

    add page home v1

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from scraping.models import Vacancy
+
+
+def index(request):
+    qs = Vacancy.objects.all()
+
+    context = {
+        "obj_list": qs
+    }
+    return render(request, "scraping/home.html", context=context)
