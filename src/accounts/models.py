26fc025d6203df3881653078d2080a759bfb6123
skantor <serhiykantor@gmail.com>
2022-12-11 02:01:37 +0200

    create new app accounts

---
+++ src/accounts/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
